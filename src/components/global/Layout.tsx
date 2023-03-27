e7361a166997674ab92cd3f25805f937133ccdad
Fray Dalton <fray117@exploiter.id>
2023-03-27 16:53:27 +0700

    add layouting

---
+++ src/components/global/Layout.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import NavBar from '@/components/global/NavBar'
+import Footer from '@/components/global/Footer'
+import { ReactNode } from 'react'
+
+export default function Layout({ children }: { children: ReactNode }) {
+	return (
+		<>
+			<Head>
+				<title>Libels Campus Fair</title>
+				<meta name="description" content="Reach your dream career through education." />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="bg-white">
+				<NavBar />
+				<div className="relative isolate pt-14">
+					{children}
+				</div>
+				<Footer />
+			</div>
+		</>
+	)
+}
