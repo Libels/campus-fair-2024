a1f99fcfb29d7a49bbd0ab4e7d95a4847a31852c
Fray Dalton <fray117@exploiter.id>
2023-05-08 23:50:14 +0700

    integrate redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,18 @@ import Layout from '@/components/global/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import store, { persistor } from '@/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</PersistGate>
+		</Provider>
 	)
 }
