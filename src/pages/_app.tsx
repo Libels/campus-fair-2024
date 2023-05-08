bb71869168ae063113cfa61879c28d80b4a972da
Fray Dalton <fray117@exploiter.id>
2023-05-09 02:01:26 +0700

    integrate next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,22 @@ import Layout from '@/components/global/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { SessionProvider } from "next-auth/react"
+
 import store, { persistor } from '@/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</PersistGate>
-		</Provider>
+		<SessionProvider session={session}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</PersistGate>
+			</Provider>
+		</SessionProvider>
 	)
 }
