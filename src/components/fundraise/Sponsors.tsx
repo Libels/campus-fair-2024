015ed9b021aea884d217043a184e4db3bf965c32
Fray Dalton <fray117@exploiter.id>
2023-05-13 18:30:29 +0700

    optimize sponsor list

--- src/components/fundraise/Sponsors.tsx
+++ src/components/fundraise/Sponsors.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 const Sponsorship = [
 	{
 		name: "Transistor",
@@ -27,11 +29,11 @@ export default function Sponsors() {
 		<div className="py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-					Didukung oleh orang keren
+					Didukung oleh partner yang keren
 				</h2>
 				<div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
 					{Sponsorship.map((sponsor) => (
-						<img
+						<Image
 							key={sponsor.name}
 							className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
 							src={sponsor.url}
