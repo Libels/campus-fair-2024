d71f27f811ab6bfa63cb14c67793d096891ea508
Fray Dalton <fray117@exploiter.id>
2023-06-27 21:48:43 +0700

    cleanup footer styling

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -10,13 +10,13 @@ export default function Footer() {
 	return (
 		<>
 			<footer className="absolute bottom-0 inset-x-0 z-50">
-				<nav className="flex items-center justify-between p-6 lg:px-8 text-sm" aria-label="Global">
+				<nav className="flex items-center justify-between p-6 lg:px-8 text-sm text-gray-500" aria-label="Global">
 					<div className="flex lg:flex-1">
 						&copy; 2023 Libels Campus Fair &times; SABAKO
 					</div>
 					<div className="hidden lg:flex lg:gap-x-12">
 						{navigation.map((item) => (
-							<Link key={item.name} href={item.href} className="text-sm leading-6 text-gray-500 hover:text-gray-700">
+							<Link key={item.name} href={item.href} className="leading-6 hover:text-gray-700">
 								{item.name}
 							</Link>
 						))}
