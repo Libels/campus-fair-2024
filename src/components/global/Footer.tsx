c278ae81a5d64cfa55c1554b3f4153f7e91bf1ad
Fray Dalton <fray117@exploiter.id>
2023-06-27 22:24:17 +0700

    add backlink

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -11,9 +11,12 @@ export default function Footer() {
 		<>
 			<footer className="absolute bottom-0 inset-x-0 z-50">
 				<nav className="flex items-center justify-between p-6 lg:px-8 text-sm text-gray-500" aria-label="Global">
-					<div className="flex lg:flex-1">
-						&copy; 2023 Libels Campus Fair &times; SABAKO
-					</div>
+					<span>
+						&copy; 2023 Libels Campus Fair &times;{' '}
+						<Link legacyBehavior href="https://sabako.id/">
+							<a target="_blank" className="text-fuchsia-600 hover:text-fuchsia-700">SABAKO</a>
+						</Link>
+					</span>
 					<div className="hidden lg:flex lg:gap-x-12">
 						{navigation.map((item) => (
 							<Link key={item.name} href={item.href} className="leading-6 hover:text-gray-700">
