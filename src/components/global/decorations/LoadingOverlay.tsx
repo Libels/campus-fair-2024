54ea8054f83f0e2a6e18bfc0cae0fad492f8a69d
Fray Dalton <fray117@exploiter.id>
2023-06-28 10:51:07 +0700

    update loading

--- src/components/global/decorations/LoadingOverlay.tsx
+++ src/components/global/decorations/LoadingOverlay.tsx
@@ -6,7 +6,7 @@ export default function LoadingOverlay() {
 
 	return (
 		loading ?
-			<div className="bg-gray-100 opacity-70 absolute w-full h-full left-0 top-0 z-[100] flex justify-center items-center" >
+			<div className="bg-gray-100 opacity-70 fixed w-full h-full left-0 top-0 z-[100] flex justify-center items-center" >
 				<div className="bg-fuchsia-300 w-10 h-10 absolute animate-ping"></div>
 			</div> : <></>
 	)
