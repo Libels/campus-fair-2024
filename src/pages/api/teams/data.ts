3eade58d4eb8dfe0740981e9a24edbbc10a48ce3
Fray Dalton <fray117@exploiter.id>
2023-10-16 23:01:18 +0700

    add teams data

---
+++ src/pages/api/teams/data.ts
@@ -0,0 +1,81 @@
+import PictureAzhari from '@/assets/university/teams/azhari.jpeg'
+import PictureEci from '@/assets/university/teams/eci.jpeg'
+import PictureFarrel from '@/assets/university/teams/farrel.jpeg'
+import PictureFray from '@/assets/university/teams/fray.jpeg'
+import PictureHanna from '@/assets/university/teams/hanna.jpeg'
+import PictureKenza from '@/assets/university/teams/kenza.jpg'
+import PictureKubil from '@/assets/university/teams/kubil.jpeg'
+import PictureMelanie from '@/assets/university/teams/melanie.jpeg'
+import PictureSyakirin from '@/assets/university/teams/syakirin.jpeg'
+import PictureTika from '@/assets/university/teams/tika.jpeg'
+import PictureOlin from '@/assets/university/teams/olin.jpg'
+import PictureMega from '@/assets/university/teams/mega.jpg'
+
+export type Data = {
+	name: string
+	role: string
+	imageUrl: any
+}
+
+export const Data = [
+	{
+		name: 'Ahmad Azhari',
+		role: 'Chief Event Director',
+		imageUrl: PictureAzhari
+	},
+	{
+		name: 'Fray',
+		role: 'Vice Event Director & CTO',
+		imageUrl: PictureFray
+	},
+	{
+		name: 'Desi Kristianti',
+		role: 'Administrative Assistant',
+		imageUrl: PictureEci
+	},
+	{
+		name: 'Elisabeth Caroline',
+		role: 'Administrative Assistant',
+		imageUrl: PictureOlin
+	},
+	{
+		name: 'Mutliqa Maimun',
+		role: 'Financial Officer',
+		imageUrl: PictureTika
+	},
+	{
+		name: 'Hanna Balqis',
+		role: 'Management Accountant',
+		imageUrl: PictureHanna
+	},
+	{
+		name: 'Mega Mawardah',
+		role: 'Social Media Manager',
+		imageUrl: PictureMega
+	},
+	{
+		name: 'Melanie Elisa Putri Saribunga',
+		role: 'Communications Specialist',
+		imageUrl: PictureMelanie
+	},
+	{
+		name: 'Ananda Syakira',
+		role: 'Event Producer',
+		imageUrl: PictureSyakirin
+	},
+	{
+		name: 'Farrel Zaindri',
+		role: 'IT Operator',
+		imageUrl: PictureFarrel
+	},
+	{
+		name: 'Kenza Ibnu',
+		role: 'Public Safety Officer',
+		imageUrl: PictureKenza
+	},
+	{
+		name: 'Nabila Salma',
+		role: 'Logistics Manager',
+		imageUrl: PictureKubil
+	},
+]
